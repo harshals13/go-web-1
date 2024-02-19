4d5b6d3e3a8fd9d3ed9877124ee3f5c20be446b7
themilebender <harshasuryavamshi@gmail.com>
2024-02-19 13:22:19 -0800

    db config updates

--- cmd/server.go
+++ cmd/server.go
@@ -1,10 +1,13 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/harshals13/go-web-1/database"
+)
 
 func main() {
 	app := fiber.New()
-
+	database.ConnectDB()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
